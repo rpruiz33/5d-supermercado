9199a1fcce2b4186ede38faf32ab1a1498caf2f4
unknown <rpruiz33@gmail.com>
2023-09-09 18:22:57 -0300

    1

---
+++ src/modelo/Carrito.java
@@ -0,0 +1,12 @@
+package modelo;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public class Carrito {
+private int idCarrito;
+private LocalDate fecha;
+private LocalTime hora;
+private List <ItemCarrito> lst; 
+private Cliente cliente;
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module eje5dSupermercado {
+}
